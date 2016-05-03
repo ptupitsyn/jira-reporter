280b167ba68a1f8ef24055165b6ab8c38f8be352
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-05-03 22:26:57 +0300

    Measure execution time

--- JiraFsharpService/Program.fs
+++ JiraFsharpService/Program.fs
@@ -4,25 +4,32 @@ open System.Diagnostics
 open Suave
 open Suave.Successful
 
+let measureTime f =
+    let sw = Stopwatch.StartNew()
+    let res = f()
+    let elapsed = sw.Elapsed
+    (res, elapsed)
+
 [<EntryPoint>]
 let main argv = 
     printfn "Starting Jira test"
 
-    let monitor = new Object();
+    let monitor = new Object()
     let mutable cachedReport = ""
     let mutable lastUpdated = DateTime.MinValue
 
-    let getReport () =                 
+    let getReport() =                 
         match lastUpdated with
             | x when x > DateTime.Now.AddSeconds(-5.0) -> cachedReport
             | _ -> 
-                let issues = Jira.getIssues "-12h"
+                let (issues, elapsed) = measureTime(fun() -> Jira.getIssues "-12h")
                 let title = Jira.getTitle()
                 lastUpdated <- DateTime.Now
-                cachedReport <- sprintf "<h1>%s</h1>%s<br/><hr/>Last updated: %A" title issues lastUpdated
+
+                cachedReport <- sprintf "<h1>%s</h1>%s<br/><hr/>Last updated at %A in %A" title issues lastUpdated elapsed
                 cachedReport
 
-    let getReportSynced () = lock monitor getReport
+    let getReportSynced() = lock monitor getReport
 
     let getReportWeb : WebPart = 
         fun (ctx : HttpContext) -> 
