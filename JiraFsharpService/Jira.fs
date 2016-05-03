25571842aea59d42f2ee2b87861014c9f6feba71
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-05-03 21:27:01 +0300

    Fix error on empty report; add 5 second limit for report refresh

--- JiraFsharpService/Jira.fs
+++ JiraFsharpService/Jira.fs
@@ -6,29 +6,31 @@ module Jira =
 
     type Issues = JsonProvider<"https://issues.apache.org/jira/rest/api/latest/search?jql=project=ignite&maxResults=1&expand=changelog">
 
-    let getIssues = 
-        async {
-            let! jiraResult = Issues.AsyncLoad "https://issues.apache.org/jira/rest/api/latest/search?jql=project=ignite AND updated>-12h AND status not in (open)&maxResults=50&expand=changelog"
+    let getIssues period = 
+        let url = sprintf "https://issues.apache.org/jira/rest/api/latest/search?jql=project=ignite AND updated>%s AND status not in (open)&maxResults=50&expand=changelog" period
+        let jiraResult = Issues.Load url
 
-            let concat acc x = acc + "<br />" + x
-            let concat2 acc x = acc + "<br /><br />" + x
-            let makeHeader x = "<h3>" + x + "</h3>"
-            let makeLink text url = sprintf "<a href='%s'>%s</a>" url text
-            let formatIssue (key, sum, status) = makeLink (key + " " + sum) (sprintf "https://issues.apache.org/jira/browse/%s" key) + " - " + status
+        let concat acc x = acc + "<br />" + x
+        let concat2 acc x = acc + "<br /><br />" + x
+        let makeHeader x = "<h3>" + x + "</h3>"
+        let makeLink text url = sprintf "<a href='%s'>%s</a>" url text
+        let formatIssue (key, sum, status) = makeLink (key + " " + sum) (sprintf "https://issues.apache.org/jira/browse/%s" key) + " - " + status
 
-            return
-                jiraResult.Issues
-                    |> Seq.collect (fun issue -> 
-                        issue.Changelog.Histories 
-                            |> Seq.where (fun hist -> (System.DateTime.Now - hist.Created).Hours < 12)
-                            |> Seq.map (fun hist -> hist.Author.DisplayName)
-                            |> Seq.distinct
-                            |> Seq.map (fun author -> (author, (issue.Key, issue.Fields.Summary, issue.Fields.Status.Name)))
-                        )
-                    |> Seq.groupBy (fun (person, _) -> makeHeader person)
-                    |> Seq.sortBy (fun (person, _) -> person)
-                    |> Seq.map (fun (person, issues) -> person + (issues |> Seq.map (snd >> formatIssue) |> Seq.reduce concat))
-                    |> Seq.reduce concat2
-        }
+        let issues = jiraResult.Issues
 
-    let getTitle = DateTime.Now |> (fun dt -> (sprintf "DAILY STATUS (%i/%i/%i)" dt.Month dt.Day dt.Year))
+        match issues with
+            | [||] -> "No issues found for the given period"
+            | x -> x 
+                |> Seq.collect (fun issue -> 
+                    issue.Changelog.Histories 
+                        |> Seq.where (fun hist -> (System.DateTime.Now - hist.Created).Hours < 12)
+                        |> Seq.map (fun hist -> hist.Author.DisplayName)
+                        |> Seq.distinct
+                        |> Seq.map (fun author -> (author, (issue.Key, issue.Fields.Summary, issue.Fields.Status.Name)))
+                    )
+                |> Seq.groupBy (fun (person, _) -> makeHeader person)
+                |> Seq.sortBy (fun (person, _) -> person)
+                |> Seq.map (fun (person, issues) -> person + (issues |> Seq.map (snd >> formatIssue) |> Seq.reduce concat))
+                |> Seq.reduce concat2
+
+    let getTitle() = DateTime.Now |> (fun dt -> (sprintf "DAILY STATUS (%i/%i/%i)" dt.Month dt.Day dt.Year))
--- JiraFsharpService/Program.fs
+++ JiraFsharpService/Program.fs
@@ -1,4 +1,5 @@
 ﻿open JiraFsharpService
+open System
 open Suave
 open Suave.Filters
 open Suave.Successful
@@ -8,16 +9,29 @@ open Suave.Operators
 let main argv = 
     printfn "Starting Jira test"
 
-    let getReport : WebPart =
-        fun (ctx : HttpContext) ->
-            async {
-                let! issuesHtml = Jira.getIssues
-                let html = sprintf "<h1>%s</h1>%s" Jira.getTitle issuesHtml
-                return! OK html ctx
-            }
+    let monitor = new Object();
+    let mutable cachedReport = ""
+    let mutable lastUpdated = DateTime.MinValue
+
+    let getReport () = 
+        lock monitor (fun () -> 
+                match lastUpdated with
+                    | x when x > DateTime.Now.AddSeconds(-5.0) -> cachedReport
+                    | _ -> 
+                        let issues = Jira.getIssues "-12h"
+                        let title = Jira.getTitle()
+                        lastUpdated <- DateTime.Now
+                        cachedReport <- sprintf "<h1>%s</h1>%s<br/><hr/>Last updated: %A" title issues lastUpdated
+                        cachedReport
+            )
+
+    let getReportWeb : WebPart = 
+        fun (ctx : HttpContext) -> 
+            let html = getReport()
+            OK html ctx
 
     System.Diagnostics.Process.Start("http://localhost:8083") |> ignore
 
-    startWebServer defaultConfig getReport
+    startWebServer defaultConfig getReportWeb
 
     0 // return an integer exit code
