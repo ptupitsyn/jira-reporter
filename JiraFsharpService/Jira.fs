26a9ca4d0dacdca6d47e6ee36db349c400178a82
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-05-04 15:47:03 +0300

    Implemented "pending patches" section

--- JiraFsharpService/Jira.fs
+++ JiraFsharpService/Jira.fs
@@ -17,7 +17,7 @@ module Jira =
         let onReviewUrl = sprintf "%ssearch?jql=project=ignite AND status = 'Patch Available'&maxResults=100&expand=changelog" apiUrl
         
         let jiraResult = Issues.Load url
-        //let onReview = Issues.Load onReviewUrl
+        let onReview = Issues.Load onReviewUrl
 
         let concat acc x = acc + "<br />" + x
         let concat2 acc x = acc + "<br /><br />" + x
@@ -33,10 +33,24 @@ module Jira =
                     | _ -> "Black"
             sprintf "<span style='color:%s'>%s</span>" color status
 
-        let formatIssue (key, sum, status) = 
-            let summary = key + " " + sum
-            let url = sprintf "https://issues.apache.org/jira/browse/%s" key
-            makeLink summary url + " - " + formatStatus status
+        let formatIssueEx includeStatus (issue : Issues.Issue) = 
+            let summary = issue.Key + " " + issue.Fields.Summary
+            let url = sprintf "https://issues.apache.org/jira/browse/%s" issue.Key
+            makeLink summary url + (if includeStatus then " - " + formatStatus issue.Fields.Status.Name else "")
+
+        let formatIssue = formatIssueEx true
+        let formatIssueNoStatus = formatIssueEx false
+
+        let historyIsPatch (hist : Issues.History) = 
+            hist.Items |> Seq.exists (fun x -> (x.Field = "status" && x.ToString = "Patch Available"))
+
+        let findPatchAuthor (issue : Issues.Issue) = 
+            (issue.Changelog.Histories |> Seq.filter historyIsPatch |> Seq.last).Author.DisplayName
+
+        let pendingPatches = 
+            onReview.Issues 
+                |> Seq.groupBy findPatchAuthor
+                |> Map.ofSeq
 
         let issues = jiraResult.Issues
 
@@ -48,11 +62,18 @@ module Jira =
                         |> Seq.where (fun hist -> (System.DateTime.Now - hist.Created).Hours < 12)
                         |> Seq.map (fun hist -> hist.Author.DisplayName)
                         |> Seq.distinct
-                        |> Seq.map (fun author -> (author, (issue.Key, issue.Fields.Summary, issue.Fields.Status.Name)))
+                        |> Seq.map (fun author -> (author, issue))
                     )
-                |> Seq.groupBy (fun (person, _) -> makeHeader person)
+                |> Seq.groupBy (fun (person, _) -> person)
                 |> Seq.sortBy (fun (person, _) -> person)
-                |> Seq.map (fun (person, issues) -> person + (issues |> Seq.map (snd >> formatIssue) |> Seq.reduce concat))
+                |> Seq.map 
+                    (fun (person, issuePairs) -> 
+                        let issues = issuePairs |> Seq.map snd |> List.ofSeq
+                        let reviews = match pendingPatches.TryFind person with
+                            | Some(reviews) -> concat "<br/><br/><b>Pending Patches</b>" (reviews |> Seq.filter (fun x -> issues |> List.exists (fun y -> y.Id = x.Id) |> not) |> Seq.map formatIssueNoStatus |> Seq.reduce concat)
+                            | _ -> ""
+                        (makeHeader person) + (issues |> Seq.map formatIssue |> Seq.reduce concat) + reviews
+                    )
                 |> Seq.reduce concat2
 
     let getTitle() = DateTime.Now |> (fun dt -> (sprintf "DAILY STATUS (%i/%i/%i)" dt.Month dt.Day dt.Year))
