2da7876e46be36a6deb59de91f35096669f6a917
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-05-04 14:16:27 +0300

    Add html formatting

--- JiraFsharpService/Program.fs
+++ JiraFsharpService/Program.fs
@@ -13,6 +13,18 @@ let measureTime f =
 [<EntryPoint>]
 let main argv = 
     printfn "Starting Jira test"
+    
+    // TODO: Extract HTML logic to a separate module
+    let header = """ 
+        <html>
+            <head>
+                <meta charset="utf-8" />
+                <title>JIRA Report</title>
+                <style type="text/css">body{margin:20px 40px}</style>
+            </head>
+            <body> """
+
+    let footer = "</body></html>"
 
     let monitor = new Object()
     let mutable cachedReport = ""
@@ -26,7 +38,7 @@ let main argv =
                 let title = Jira.getTitle()
                 lastUpdated <- DateTime.Now
 
-                cachedReport <- sprintf "<h1>%s</h1>%s<br/><hr/>Last updated at %A in %A" title issues lastUpdated elapsed
+                cachedReport <- sprintf "%s<h1>%s</h1>%s<br/><hr/>Last updated at %A in %A%s" header title issues lastUpdated elapsed footer
                 cachedReport
 
     let getReportSynced() = lock monitor getReport
