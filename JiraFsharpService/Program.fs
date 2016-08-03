4c7441d8d0a46fbfaeec76d972197b108d1ff87d
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-03 15:25:37 +0300

    Windows service implemented

--- JiraFsharpService/Program.fs
+++ JiraFsharpService/Program.fs
@@ -1,10 +1,14 @@
 ﻿open System.Diagnostics
 open JiraFsharpService
+open System.ServiceProcess;
 
 [<EntryPoint>]
 let main argv = 
-    Process.Start("http://localhost:3443") |> ignore
+    
+    // Uncomment to run in console
+    //Process.Start("http://localhost:3443") |> ignore
+    //WebServer.runWebServer()
 
-    WebServer.runWebServer()
+    ServiceBase.Run [| new WindowsService() :> ServiceBase |]
 
     0 // return an integer exit code
