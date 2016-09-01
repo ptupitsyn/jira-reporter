ae27f8130408157f8be91a1211a79f2fd726d7b3
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-09-01 11:00:52 +0300

    Autodetect console and service mode on start to simplify debugging

--- JiraFsharpService/Program.fs
+++ JiraFsharpService/Program.fs
@@ -1,4 +1,5 @@
-﻿open System.Diagnostics
+﻿open System
+open System.Diagnostics
 open JiraFsharpService
 open System.ServiceProcess;
 open System.Threading;
@@ -6,13 +7,13 @@ open System.Threading;
 [<EntryPoint>]
 let main argv = 
     
-    // Uncomment to run in console
-    (*
-    Process.Start("http://localhost:3443/?showComments=false&personFilter=tupitsyn") |> ignore
-    WebServer.runWebServer()
-    Thread.Sleep(Timeout.Infinite)
-    *)
-
-    ServiceBase.Run [| new WindowsService() :> ServiceBase |]
+    if Environment.UserInteractive then
+        // Run in console
+        Process.Start("http://localhost:3443/?showComments=false&personFilter=tupitsyn") |> ignore
+        WebServer.runWebServer()
+        Thread.Sleep(Timeout.Infinite)
+    else
+        // Start a windows service
+        ServiceBase.Run [| new WindowsService() :> ServiceBase |]
 
     0 // return an integer exit code
